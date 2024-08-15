70ebde2e180c8942d6ee969266d1e49248bbb1f4
Daniel <daniel.40.pompa@gmail.com>
2024-08-16 00:06:22 +0200

    feat: add styled logging utility function

---
+++ src/utils/log-styled.ts
@@ -0,0 +1,5 @@
+/**
+ * Logs the given message to the console with a blue color and bold font style.
+ * @param message The message to be logged.
+ */
+export const logStyled = (message: string) => console.log('%c' + message, 'color: #1e3a8a; font-weight: bold;');
